704ad5b7db13b0c599cd97ac323f7a8b13ad6043
egor-no <e.nosko93@ya.ru>
2025-12-25 15:27:14 +0500

    backend spring init2

---
+++ back-and-front-version/backend/winylka/src/main/java/winylka/WinylkaApplication.java
@@ -0,0 +1,13 @@
+package winylka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinylkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WinylkaApplication.class, args);
+	}
+
+}
---
+++ back-and-front-version/backend/winylka/src/test/java/winylka/WinylkaApplicationTests.java
@@ -0,0 +1,13 @@
+package winylka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinylkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
